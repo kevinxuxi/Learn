1b34585160860f5a5793bc52b1f9e6d50b4d6550
xuxi <981821764@qq.com>
2020-07-10 14:59:27 +0800

    Initial Commit

---
+++ Learn/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Learn
+//
+//  Created by mac on 2020/7/10.
+//  Copyright © 2020 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Learn/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Learn
+//
+//  Created by mac on 2020/7/10.
+//  Copyright © 2020 mac. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
